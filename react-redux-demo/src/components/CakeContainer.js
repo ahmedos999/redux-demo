853fb2cbe87250eccdc4d21516d3b8ece7ea5496
ahmed osman <ahmedos099912@gmail.com>
2023-09-17 17:43:24 +0200

    working first state cake store

--- react-redux-demo/src/components/CakeContainer.js
+++ react-redux-demo/src/components/CakeContainer.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { buyCake } from '../redux'
+import { connect } from 'react-redux'
 
-function CakeContainer() {
+function CakeContainer(props) {
   return (
     <div>
-        <h2>Number of Cake</h2>
-        <button>buy cake</button>
+        <h2>Number of Cake - {props.numOfCakes}</h2>
+        <button onClick={props.buyCake}>buy cake</button>
     </div>
   )
 }
+const mapStateToProps = state =>{
+  return{
+    numOfCakes:state.numOfCakes
+  }
+}
 
-export default CakeContainer
+const mapDispatchToprops = dispatch =>{
+  return{
+    buyCake:()=>dispatch(buyCake())
+  }
+}
+export default connect(mapStateToProps,mapDispatchToprops)(CakeContainer)
