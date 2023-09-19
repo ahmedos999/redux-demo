ee47e36364775b038b0928f0a3fd1a06d5809aa3
ahmed osman <ahmedos099912@gmail.com>
2023-09-19 13:40:52 +0200

    devtool store

--- react-redux-demo/src/redux/store.js
+++ react-redux-demo/src/redux/store.js
@@ -1,11 +1,12 @@
 import { createStore,applyMiddleware} from 'redux'
 import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension';
 // import cakeReducer from './cakes/ReducerCake'
 // import icecreamReducer from './icecream/icecreamReducer'
 
 import rootReduacer from './rootReducer'
 
 
-const store = createStore(rootReduacer,applyMiddleware(logger))
+const store = createStore(rootReduacer,composeWithDevTools(applyMiddleware(logger)))
 
 export default store
