235b395418db2a27af9948ae3cb3a4bb8dea707e
ahmed osman <ahmedos099912@gmail.com>
2023-09-24 11:12:46 +0200

    deleted appwrite

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 // import WeatherContainer from './components/WeatherContainer';
-import { Appwrite } from 'appwrite';
+import { Client, Databases, Query,Account, ID  } from "appwrite";
 // import CakeContainer from './components/CakeContainer';
 // import HookCakeContainer from './components/HookCakeContainer';
 // import IcecreamContainer from './components/IcecreamContainer';
@@ -11,14 +11,41 @@ import { Appwrite } from 'appwrite';
 
 import store from './redux/store'
 import { Provider } from 'react-redux';
-import Tasks from './components/AppwriteTest';
+
 
 function App() {
-  const appwrite = new Appwrite();
+  const client = new Client()
 
-appwrite
+  client
   .setEndpoint('https://cloud.appwrite.io/v1') // Replace with your Appwrite server endpoint
-  .setProject('650f026a6ac5b8e7bd95') // Replace with your Appwrite project ID
+  .setProject('650f026a6ac5b8e7bd95')
+
+  const account = new Account(client);
+
+  account.create(
+    ID.unique(),
+    'test@example.com',
+    '123321',
+    'peyton list'
+).then(response => {
+    console.log(response);
+}, error => {
+    console.log(error);
+});
+  
+  const databases = new Databases(client)
+
+  databases.listDocuments(
+    'todoDB101',
+    'todocollectionDB101',
+    [
+      Query.equal('action','testing'),
+    ]
+);
+
+console.log(databases)
+  
+  // Replace with your Appwrite project ID
   // .setKey('your-api-key');
   return (
     <Provider store={store}>
@@ -31,7 +58,6 @@ appwrite
      <NewCakeContainer></NewCakeContainer>
     <UserContainerNew></UserContainerNew> */}
     {/* <WeatherContainer></WeatherContainer> */}
-    <Tasks></Tasks>
     </div>
     </Provider>
   );
